d9f267b29253ccecca7dfde7da2a37f4ef53498e
arildo <arildohp@gmail.com>
2024-04-11 09:43:23 -0300

    construindo o card de contatos

---
+++ src/components/Contato/index.tsx
@@ -0,0 +1,15 @@
+import * as S from './styles'
+
+const Contato = () => (
+  <S.Card>
+    <S.Titulo>Contato</S.Titulo>
+    <S.Tag>trabalho</S.Tag>
+    <S.Descricao />
+    <S.BarraAcoes>
+      <S.Botao>Editar</S.Botao>
+      <S.Botao>Remover</S.Botao>
+    </S.BarraAcoes>
+  </S.Card>
+)
+
+export default Contato
