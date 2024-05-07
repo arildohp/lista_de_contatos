c6867da30e05f664b4a6a5f911b988fc84d8996e
arildo <arildohp@gmail.com>
2024-05-07 16:24:08 -0300

    configurando react-router-dom

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import BarraLateral from '../../Container/BarraLateral'
+import ListaDeContatos from '../../Container/ListaDeContatos'
+
+const Home = () => (
+  <>
+    <BarraLateral />
+    <ListaDeContatos />
+  </>
+)
+
+export default Home
