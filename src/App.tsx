66f4acd964967f39642113aae0b7428c26d575f4
arildo <arildohp@gmail.com>
2024-06-15 07:24:00 -0300

    projeto finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import EstiloGlobal, { Container } from './styles'
 
 import store from './store'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Cadastro from './pages/Cadastro'
 
 const rotas = createBrowserRouter([
